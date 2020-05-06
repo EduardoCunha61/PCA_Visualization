53fb17101b058357c1364b7bc7cdeba180267d2b
Hodor61 <eduardodelimacunha@gmail.com>
2020-05-06 15:31:33 +0100

    Eliminated old files

--- checkcsv.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open("excelPCA.csv",'r') as f:
-    reader = csv.reader(f)
-    linenumber = 1
-    try:
-        for row in reader:
-            linenumber += 1
-    except Exception as e:
-        print (("Error line %d: %s %s" % (linenumber, str(type(e)), e.message)))
